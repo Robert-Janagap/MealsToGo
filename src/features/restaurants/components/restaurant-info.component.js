f25fb616e89f85aba92bd002d80ea27062f7b6c8
Robert Janagap <janagaprobert@gmail.com>
2022-05-23 12:21:51 +0800

    restaurant info setup #2

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,3 +1,16 @@
 import react from 'react';
 import { Text } from 'react-native-paper';
-export const RestaurantInfo = () => <Text>Restaurant info</Text>
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = 'Some Restaurant',
+    icon,
+    photos = [
+      'https://www.foodiesfeed.com/wp-content/uploads/2021/04/potato-quinoa-patties.jpg'
+    ],
+    address = '100 some random street',
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily
+  } = restaurant;
+  return <Text>{name}</Text>;
+};
